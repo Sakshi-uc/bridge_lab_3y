b917a5cf3f9a9f59e1edd6dbb7406a30cb18ff7d
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:23:54 +0530

    Update MeanHeight.java

--- level 1/MeanHeight.java
+++ level 1/MeanHeight.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 
-public class MeanHeight {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double[] heights = new double[11];
-        double sum = 0;
-
-        System.out.println("Enter the heights of 11 players:");
-        for (int i = 0; i < heights.length; i++) {
-            heights[i] = input.nextDouble();
-            sum += heights[i];
-        }
-
-        double mean = sum / heights.length;
-        System.out.println("Mean height of the football team: " + mean);
-
-        input.close();
-    }
+public class MeanHeight{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      double [] h=new double[11];
+      double total=0;
+      System.out.println("Enter the heights of 11 players :");
+      for(int i=0;i<h.length;i++){
+         h[i]=sc.nextDouble();
+         total+=h[i];
+      }
+      double avg= total / h.length;
+      System.out.println("Mean height of the football team : "+avg);
+      sc.close();
+   }
 }
