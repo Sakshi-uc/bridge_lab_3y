aa96fc993bfcc597327a888d9056cc3b7a63693b
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:22:18 +0530

    Update Factors.java

--- level 1/Factors.java
+++ level 1/Factors.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
 
-public class Factors {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = input.nextInt();
-
-        int maxFactor = 10;
-        int[] factors = new int[maxFactor];
-        int index = 0;
-
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) {
-                if (index == maxFactor) {
-                    maxFactor *= 2;
-                    int[] temp = new int[maxFactor];
-                    System.arraycopy(factors, 0, temp, 0, factors.length);
-                    factors = temp;
-                }
-                factors[index++] = i;
+public class Factors{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a number :");
+      int num=sc.nextInt();
+      int max=10;
+      int [] fact=new int[max];
+      int k=0;
+      for(int i=1;i<=num;i++){
+         if(num % i==0){
+            if(k==max){
+               max*=2;
+               int [] tmp=new int[max];
+               System.arraycopy(fact,0,tmp,0,fact.length);
+               fact=tmp;
             }
-        }
-
-        System.out.println("Factors of " + number + ":");
-        for (int i = 0; i < index; i++) {
-            System.out.print(factors[i] + " ");
-        }
-
-        input.close();
-    }
+            fact[k]=i;
+            k++;
+         }
+      }
+      System.out.println("Factors of "+num+" :");
+      for(int i=0;i<k;i++){
+         System.out.print(fact[i]+" ");
+      }
+      sc.close();
+   }
 }
