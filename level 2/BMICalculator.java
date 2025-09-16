d81bbe6e0b83ae16e48c09a835462169a641e13e
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:27:22 +0530

    Update BMICalculator.java

--- level 2/BMICalculator.java
+++ level 2/BMICalculator.java
@@ -1,35 +1,32 @@
-package Array_practice_level_2;
-
 import java.util.Scanner;
 
-public class BMICalculator {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number of persons: ");
-        int n = input.nextInt();
-
-        double[] height = new double[n];
-        double[] weight = new double[n];
-        double[] bmi = new double[n];
-        String[] status = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter height (m) and weight (kg) for person " + (i + 1) + ":");
-            height[i] = input.nextDouble();
-            weight[i] = input.nextDouble();
-
-            bmi[i] = weight[i] / (height[i] * height[i]);
-            if (bmi[i] < 18.5) status[i] = "Underweight";
-            else if (bmi[i] < 25) status[i] = "Normal";
-            else if (bmi[i] < 30) status[i] = "Overweight";
-            else status[i] = "Obese";
-        }
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Person " + (i + 1) + ": Height=" + height[i] + "m, Weight=" + weight[i] +
-                    "kg, BMI=" + bmi[i] + ", Status=" + status[i]);
-        }
-
-        input.close();
-    }
+public class NumDivider{
+   public static void main(String[] args){
+      Scanner s=new Scanner(System.in);
+      System.out.print("Enter a natural number: ");
+      int num=s.nextInt();
+      if(num<=0){
+         System.out.println("Error: give a natural number");
+         return;
+      }
+      int [] oddList=new int[num/2+1];
+      int [] evenList=new int[num/2+1];
+      int o=0,e=0;
+      for(int i=1;i<=num;i++){
+         if(i%2==0){
+            evenList[e++]=i;
+         }else{
+            oddList[o++]=i;
+         }
+      }
+      System.out.println("Odd numbers:");
+      for(int j=0;j<o;j++){
+         System.out.print(oddList[j]+" ");
+      }
+      System.out.println("\nEven numbers:");
+      for(int k=0;k<e;k++){
+         System.out.print(evenList[k]+" ");
+      }
+      s.close();
+   }
 }
