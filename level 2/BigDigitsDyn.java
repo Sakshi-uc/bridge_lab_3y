34a639e05e04a972a16e2c4bd629d7d620eaa82c
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:43:08 +0530

    Update and rename LargestDigitsDynamic.java to BigDigitsDyn.java

---
+++ level 2/BigDigitsDyn.java
@@ -0,0 +1,36 @@
+package Array_practice_level_2;
+
+import java.util.Scanner;
+
+public class BigDigitsDyn{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int n=sc.nextInt();
+      int max=10;
+      int [] digs=new int[max];
+      int k=0;
+      while(n!=0){
+         if(k==max){
+            max+=10;
+            int [] tmp=new int[max];
+            System.arraycopy(digs,0,tmp,0,digs.length);
+            digs=tmp;
+         }
+         digs[k++]=n%10;
+         n/=10;
+      }
+      int big=0,sec=0;
+      for(int i=0;i<k;i++){
+         if(digs[i]>big){
+            sec=big;
+            big=digs[i];
+         }else if(digs[i]>sec && digs[i]!=big){
+            sec=digs[i];
+         }
+      }
+      System.out.println("Largest digit : "+big);
+      System.out.println("Second largest : "+sec);
+      sc.close();
+   }
+}
