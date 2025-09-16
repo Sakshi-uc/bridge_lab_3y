183472b9aee142ec7f166d91932473481a522a91
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:28:28 +0530

    Update BMICalculator2D.java

--- level 2/BMICalculator2D.java
+++ level 2/BMICalculator2D.java
@@ -2,41 +2,33 @@ package Array_practice_level_2;
 
 import java.util.Scanner;
 
-public class BMICalculator2D {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number of persons: ");
-        int n = input.nextInt();
-
-        double[][] personData = new double[n][3]; // weight, height, BMI
-        String[] status = new String[n];
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Enter height (m) and weight (kg) for person " + (i + 1) + ":");
-            double h = input.nextDouble();
-            double w = input.nextDouble();
-
-            if (h <= 0 || w <= 0) {
-                System.out.println("Invalid input. Try again.");
-                i--;
-                continue;
-            }
-
-            personData[i][0] = w;
-            personData[i][1] = h;
-            personData[i][2] = w / (h * h);
-
-            if (personData[i][2] < 18.5) status[i] = "Underweight";
-            else if (personData[i][2] < 25) status[i] = "Normal";
-            else if (personData[i][2] < 30) status[i] = "Overweight";
-            else status[i] = "Obese";
-        }
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("Person " + (i + 1) + ": Height=" + personData[i][1] + "m, Weight=" +
-                    personData[i][0] + "kg, BMI=" + personData[i][2] + ", Status=" + status[i]);
-        }
-
-        input.close();
-    }
+public class BMIcalc{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter number of persons : ");
+      int n=sc.nextInt();
+      double [][] data=new double[n][3]; 
+      String [] stat=new String[n];
+      for(int i=0;i<n;i++){
+         System.out.println("Enter height (m) and weight (kg) for person "+(i+1)+":");
+         double ht=sc.nextDouble();
+         double wt=sc.nextDouble();
+         if(ht<=0 || wt<=0){
+            System.out.println("Invalid input, try again.");
+            i--;
+            continue;
+         }
+         data[i][0]=wt;
+         data[i][1]=ht;
+         data[i][2]=wt/(ht*ht);
+         if(data[i][2]<18.5) stat[i]="Underweight";
+         else if(data[i][2]<25) stat[i]="Normal";
+         else if(data[i][2]<30) stat[i]="Overweight";
+         else stat[i]="Obese";
+      }
+      for(int j=0;j<n;j++){
+         System.out.println("Person "+(j+1)+": Height="+data[j][1]+"m, Weight="+data[j][0]+"kg, BMI="+data[j][2]+", Status="+stat[j]);
+      }
+      sc.close();
+   }
 }
