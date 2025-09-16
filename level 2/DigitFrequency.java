5fc14ae1493eff0611de029459c3217bfba40264
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:29:33 +0530

    Update DigitFrequency.java

--- level 2/DigitFrequency.java
+++ level 2/DigitFrequency.java
@@ -2,26 +2,23 @@ package Array_practice_level_2;
 
 import java.util.Scanner;
 
-public class DigitFrequency {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = input.nextInt();
-
-        int[] freq = new int[10];
-        while (number != 0) {
-            int digit = number % 10;
-            freq[digit]++;
-            number /= 10;
-        }
-
-        System.out.println("Digit Frequencies:");
-        for (int i = 0; i < 10; i++) {
-            if (freq[i] > 0) {
-                System.out.println("Digit " + i + ": " + freq[i]);
-            }
-        }
-
-        input.close();
-    }
+public class DigFreq{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a number : ");
+      int n=sc.nextInt();
+      int [] count=new int[10];
+      while(n!=0){
+         int d=n%10;
+         count[d]++ ;
+         n/=10;
+      }
+      System.out.println("Digit frequencies :");
+      for(int i=0;i<10;i++){
+         if(count[i]>0){
+            System.out.println("Digit "+i+" -> "+count[i]);
+         }
+      }
+      sc.close();
+   }
 }
