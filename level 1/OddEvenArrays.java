11e98fe986a7df418b5f0d781c7aeceda2d788eb
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-16 11:25:38 +0530

    Update OddEvenArrays.java

--- level 1/OddEvenArrays.java
+++ level 1/OddEvenArrays.java
@@ -1,38 +1,32 @@
 import java.util.Scanner;
 
-public class OddEvenArrays {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a natural number: ");
-        int number = input.nextInt();
-
-        if (number <= 0) {
-            System.out.println("Error: Enter a natural number.");
-            return;
-        }
-
-        int[] odd = new int[number / 2 + 1];
-        int[] even = new int[number / 2 + 1];
-        int oddIndex = 0, evenIndex = 0;
-
-        for (int i = 1; i <= number; i++) {
-            if (i % 2 == 0) {
-                even[evenIndex++] = i;
-            } else {
-                odd[oddIndex++] = i;
-            }
-        }
-
-        System.out.println("Odd numbers:");
-        for (int i = 0; i < oddIndex; i++) {
-            System.out.print(odd[i] + " ");
-        }
-
-        System.out.println("\nEven numbers:");
-        for (int i = 0; i < evenIndex; i++) {
-            System.out.print(even[i] + " ");
-        }
-
-        input.close();
-    }
+public class OddEvenArr{
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a natural number :");
+      int n=sc.nextInt();
+      if(n<=0){
+         System.out.println("Error : enter a natural number.");
+         return;
+      }
+      int [] odd=new int[n/2+1];
+      int [] even=new int[n/2+1];
+      int oi=0,ei=0;
+      for(int j=1;j<=n;j++){
+         if(j%2==0){
+            even[ei++]=j;
+         }else{
+            odd[oi++]=j;
+         }
+      }
+      System.out.println("Odd numbers :");
+      for(int k=0;k<oi;k++){
+         System.out.print(odd[k]+" ");
+      }
+      System.out.println("\nEven numbers :");
+      for(int m=0;m<ei;m++){
+         System.out.print(even[m]+" ");
+      }
+      sc.close();
+   }
 }
