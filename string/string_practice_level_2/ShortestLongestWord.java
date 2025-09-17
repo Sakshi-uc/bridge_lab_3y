2bebc9c25e92a06b5c8e3a618e955cfd8a73762b
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-17 11:46:54 +0530

    Update ShortestLongestWord.java

--- string/string_practice_level_2/ShortestLongestWord.java
+++ string/string_practice_level_2/ShortestLongestWord.java
@@ -3,7 +3,6 @@ package string_practice_level_2;
 import java.util.Scanner;
 
 public class ShortestLongestWord {
-
     static int getLength(String str) {
         int count = 0;
         try {
@@ -11,10 +10,8 @@ public class ShortestLongestWord {
         } catch (Exception e) {}
         return count;
     }
-
     static String[] splitWords(String text) {
-        int n = getLength(text);
-        int spaces = 0;
+        int n = getLength(text), spaces = 0;
         for (int i = 0; i < n; i++) if (text.charAt(i) == ' ') spaces++;
         String[] words = new String[spaces + 1];
         int start = 0, index = 0;
@@ -26,24 +23,5 @@ public class ShortestLongestWord {
         }
         return words;
     }
-
     static String[] shortestLongest(String[] words) {
-        String shortest = words[0], longest = words[0];
-        for (String w : words) {
-            if (getLength(w) < getLength(shortest)) shortest = w;
-            if (getLength(w) > getLength(longest)) longest = w;
-        }
-        return new String[]{shortest, longest};
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter text: ");
-        String input = sc.nextLine();
-        String[] words = splitWords(input);
-        String[] result = shortestLongest(words);
-        System.out.println("Shortest word: " + result[0]);
-        System.out.println("Longest word: " + result[1]);
-        sc.close();
-    }
-}
+        String
