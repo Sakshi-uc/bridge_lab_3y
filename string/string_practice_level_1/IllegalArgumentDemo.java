d1a83203cfa2e52ba527eebdd3cf0d2096393246
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-17 11:40:00 +0530

    Update IllegalArgumentDemo.java

--- string/string_practice_level_1/IllegalArgumentDemo.java
+++ string/string_practice_level_1/IllegalArgumentDemo.java
@@ -1,12 +1,12 @@
 package string_practice_level_1;
 
-public class IllegalArgumentDemo {
-    public static void main(String[] args) {
-        try {
-            String str = "abcdef";
-            System.out.println(str.substring(5, 2)); // start > end
-        } catch (IllegalArgumentException e) {
-            System.out.println("Caught IllegalArgumentException: " + e.getMessage());
-        }
-    }
+public class IllegalArgumentDemo{
+   public static void main(String[] args){
+      try{
+         String txt="abcdef";
+         System.out.println(txt.substring(5,2));
+      }catch(IllegalArgumentException ex){
+         System.out.println("Caught IllegalArgumentException : "+ex.getMessage());
+      }
+   }
 }
