2ef22d9500b69e09878f9b35ab7f71a26b907184
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-17 11:39:03 +0530

    Update ArrayIndexOutOfBoundsDemo.java

--- string/string_practice_level_1/ArrayIndexOutOfBoundsDemo.java
+++ string/string_practice_level_1/ArrayIndexOutOfBoundsDemo.java
@@ -1,12 +1,12 @@
 package string_practice_level_1;
 
-public class ArrayIndexOutOfBoundsDemo {
-    public static void main(String[] args) {
-        try {
-            String[] names = {"A", "B", "C"};
-            System.out.println(names[5]); // invalid index
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
-        }
-    }
+public class ArrayIndexOutOfBoundsDemo{
+   public static void main(String[] args){
+      try{
+         String [] arr={"A","B","C"};
+         System.out.println(arr[5]); 
+      }catch(ArrayIndexOutOfBoundsException ex){
+         System.out.println("Caught ArrayIndexOutOfBoundsException : "+ex.getMessage());
+      }
+   }
 }
