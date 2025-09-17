f1d403691f1df471404255cbc1567dfd60cb06c6
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-17 11:39:34 +0530

    Update CharArrayDemo.java

--- string/string_practice_level_1/CharArrayDemo.java
+++ string/string_practice_level_1/CharArrayDemo.java
@@ -2,35 +2,31 @@ package string_practice_level_1;
 
 import java.util.Scanner;
 
-public class CharArrayDemo {
+public class CharArrayDemo{
 
-    // User-defined method to convert string to char array
-    public static char[] toCharArrayManual(String text) {
-        char[] arr = new char[text.length()];
-        for (int i = 0; i < text.length(); i++) {
-            arr[i] = text.charAt(i);
-        }
-        return arr;
-    }
+   public static char[] makeCharArr(String s){
+      char [] arr=new char[s.length()];
+      for(int i=0;i<s.length();i++){
+         arr[i]=s.charAt(i);
+      }
+      return arr;
+   }
 
-    // Compare two char arrays
-    public static boolean compareArrays(char[] a1, char[] a2) {
-        if (a1.length != a2.length) return false;
-        for (int i = 0; i < a1.length; i++) {
-            if (a1[i] != a2[i]) return false;
-        }
-        return true;
-    }
+   public static boolean sameArr(char[] x,char[] y){
+      if(x.length!=y.length) return false;
+      for(int j=0;j<x.length;j++){
+         if(x[j]!=y[j]) return false;
+      }
+      return true;
+   }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String text = sc.next();
-
-        char[] userDefined = toCharArrayManual(text);
-        char[] builtIn = text.toCharArray();
-
-        System.out.println("Are both arrays equal? " + compareArrays(userDefined, builtIn));
-    }
+   public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a string : ");
+      String str=sc.next();
+      char [] arr1=makeCharArr(str);
+      char [] arr2=str.toCharArray();
+      System.out.println("Are both arrays equal ? "+sameArr(arr1,arr2));
+      sc.close();
+   }
 }
