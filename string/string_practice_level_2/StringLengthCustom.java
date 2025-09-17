69b58c9c064f2771227cf73b4959ad419c7adcc2
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-17 11:49:27 +0530

    Update StringLengthCustom.java

--- string/string_practice_level_2/StringLengthCustom.java
+++ string/string_practice_level_2/StringLengthCustom.java
@@ -3,26 +3,26 @@ package string_practice_level_2;
 import java.util.Scanner;
 
 public class StringLengthCustom {
-    static int getLength(String str) {
-        int count = 0;
+    static int findLen(String txt) {
+        int counter = 0;
         try {
             while (true) {
-                str.charAt(count);
-                count++;
+                txt.charAt(counter);
+                counter++;
             }
-        } catch (Exception e) {
+        } catch (Exception ex) {
         }
-        return count;
+        return counter;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String input = sc.next();
-        int customLength = getLength(input);
-        int builtinLength = input.length();
-        System.out.println("Custom Length: " + customLength);
-        System.out.println("Built-in Length: " + builtinLength);
-        sc.close();
+        String data = reader.next();
+        int calcLen = findLen(data);
+        int libLen = data.length();
+        System.out.println("Custom Length: " + calcLen);
+        System.out.println("Built-in Length: " + libLen);
+        reader.close();
     }
 }
