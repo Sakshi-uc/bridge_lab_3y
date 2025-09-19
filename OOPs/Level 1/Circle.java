901f5e4b96a53647dc11bb3e185017dc8f2ca641
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-19 22:51:10 +0530

    Update Circle.java

--- OOPs/Level 1/Circle.java
+++ OOPs/Level 1/Circle.java
@@ -1,32 +1,32 @@
 import java.util.Scanner;
 
 public class Circle {
-	double radius;
-	
-	Circle(double radius){
-		this.radius = radius;
-	}
-	
-	public void areaCircle() {
-		System.out.printf("Area of circle: %.4f", (Math.PI * radius * radius));
-	}
-	
-	public void circumferenceCircle() {
-		System.out.printf("\nCircumference of circle: %.4f ", (2 * Math.PI * radius));
-	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter the radius of the circle: ");
-		double radius = scanner.nextDouble();
-		
-		Circle circle = new Circle(radius);
-		
-		System.out.println("\n--- Circle Calculations ---");
-		circle.areaCircle();
-		circle.circumferenceCircle();
-		
-		scanner.close();
-	}
+    double circleRadius;
+
+    Circle(double circleRadius) {
+        this.circleRadius = circleRadius;
+    }
+
+    public void calculateArea() {
+        System.out.printf("Area of circle: %.4f", (Math.PI * circleRadius * circleRadius));
+    }
+
+    public void calculateCircumference() {
+        System.out.printf("\nCircumference of circle: %.4f ", (2 * Math.PI * circleRadius));
+    }
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double enteredRadius = inputScanner.nextDouble();
+
+        Circle circleObj = new Circle(enteredRadius);
+
+        System.out.println("\n--- Circle Calculations ---");
+        circleObj.calculateArea();
+        circleObj.calculateCircumference();
+
+        inputScanner.close();
+    }
 }
