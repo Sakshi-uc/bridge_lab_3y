068499297312e302bdede39c8a1c7e9bf5d5d467
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-19 22:53:36 +0530

    Update HandBook.java

--- OOPs/Level 1/HandBook.java
+++ OOPs/Level 1/HandBook.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 
 public class HandBook {
-	String bookTitle;
-	String bookAuthor;
-	double price;
-	
-	HandBook(String bookTitle, String bookAuthor, double price){
-		this.bookTitle = bookTitle;
-		this.bookAuthor = bookAuthor;
-		this.price = price;
-	}
-	
-	public void displayBookDetails() {
-		System.out.println("\n--- Book Details ---");
-		System.out.println("Title of the book: " + bookTitle);
-		System.out.println("Author of the book: " + bookAuthor);
-		System.out.println("Price of the book: " + price);
-	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter Book Title: ");
-		String title = scanner.nextLine();
-		
-		System.out.print("Enter Book Author: ");
-		String author = scanner.nextLine();
-		
-		System.out.print("Enter Book Price: ");
-		double price = scanner.nextDouble();
-		
-		HandBook book = new HandBook(title, author, price);
-		book.displayBookDetails();
-		
-		scanner.close();
-	}
+    String handBookTitle;
+    String handBookAuthor;
+    double handBookPrice;
+    
+    HandBook(String handBookTitle, String handBookAuthor, double handBookPrice) {
+        this.handBookTitle = handBookTitle;
+        this.handBookAuthor = handBookAuthor;
+        this.handBookPrice = handBookPrice;
+    }
+    
+    public void showHandBookDetails() {
+        System.out.println("\n--- Book Details ---");
+        System.out.println("Title of the book: " + handBookTitle);
+        System.out.println("Author of the book: " + handBookAuthor);
+        System.out.println("Price of the book: " + handBookPrice);
+    }
+    
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        
+        System.out.print("Enter Book Title: ");
+        String enteredBookTitle = inputScanner.nextLine();
+        
+        System.out.print("Enter Book Author: ");
+        String enteredBookAuthor = inputScanner.nextLine();
+        
+        System.out.print("Enter Book Price: ");
+        double enteredBookPrice = inputScanner.nextDouble();
+        
+        HandBook handBookObj = new HandBook(enteredBookTitle, enteredBookAuthor, enteredBookPrice);
+        handBookObj.showHandBookDetails();
+        
+        inputScanner.close();
+    }
 }
