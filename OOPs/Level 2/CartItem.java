628096bf7bea988e3a7f0e4a57da184d3bce1e76
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-19 22:52:58 +0530

    Update CartItem.java

--- OOPs/Level 2/CartItem.java
+++ OOPs/Level 2/CartItem.java
@@ -1,91 +1,92 @@
 import java.util.Scanner;
 
 public class CartItem {
-	private String itemName;
-	private double price;
-	private int quantity;
+    private String cartItemName;
+    private double cartItemPrice;
+    private int cartItemQuantity;
 
-	public CartItem(String itemName, double price, int quantity) {
-		this.itemName = itemName;
-		this.price = price;
-		this.quantity = quantity;
-	}
+    public CartItem(String cartItemName, double cartItemPrice, int cartItemQuantity) {
+        this.cartItemName = cartItemName;
+        this.cartItemPrice = cartItemPrice;
+        this.cartItemQuantity = cartItemQuantity;
+    }
 
-	public void addItem(int quantityToAdd) {
-		if (quantityToAdd > 0) {
-			this.quantity += quantityToAdd;
-			System.out.println("Added " + quantityToAdd + " of " + this.itemName + " to the cart.");
-		} else {
-			System.out.println("Quantity to add must be positive.");
-		}
-	}
+    public void addToCart(int quantityToAdd) {
+        if (quantityToAdd > 0) {
+            this.cartItemQuantity += quantityToAdd;
+            System.out.println("Added " + quantityToAdd + " of " + this.cartItemName + " to the cart.");
+        } else {
+            System.out.println("Quantity to add must be positive.");
+        }
+    }
 
-	public void removeItem(int quantityToRemove) {
-		if (quantityToRemove <= 0) {
-			System.out.println("Quantity to remove must be positive.");
-		} else if (quantityToRemove > this.quantity) {
-			System.out.println("Cannot remove " + quantityToRemove + " of " + this.itemName + ". Only " + this.quantity + " in cart.");
-		} else {
-			this.quantity -= quantityToRemove;
-			System.out.println("Removed " + quantityToRemove + " of " + this.itemName + " from the cart.");
-		}
-	}
+    public void removeFromCart(int quantityToRemove) {
+        if (quantityToRemove <= 0) {
+            System.out.println("Quantity to remove must be positive.");
+        } else if (quantityToRemove > this.cartItemQuantity) {
+            System.out.println("Cannot remove " + quantityToRemove + " of " + this.cartItemName +
+                    ". Only " + this.cartItemQuantity + " in cart.");
+        } else {
+            this.cartItemQuantity -= quantityToRemove;
+            System.out.println("Removed " + quantityToRemove + " of " + this.cartItemName + " from the cart.");
+        }
+    }
 
-	public double totalCost() {
-		return this.price * this.quantity;
-	}
+    public double calculateTotalCost() {
+        return this.cartItemPrice * this.cartItemQuantity;
+    }
 
-	public void displayItem() {
-		System.out.println("\n--- Current Cart ---");
-		System.out.println("Item: " + this.itemName);
-		System.out.println("Price per item: $" + this.price);
-		System.out.println("Quantity: " + this.quantity);
-		System.out.printf("Total Cost: $%.2f\n", this.totalCost());
-	}
+    public void showCartDetails() {
+        System.out.println("\n--- Current Cart ---");
+        System.out.println("Item: " + this.cartItemName);
+        System.out.println("Price per item: $" + this.cartItemPrice);
+        System.out.println("Quantity: " + this.cartItemQuantity);
+        System.out.printf("Total Cost: $%.2f\n", this.calculateTotalCost());
+    }
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
 
-		System.out.println("--- Setup Your Shopping Cart Item ---");
-		System.out.print("Enter item name: ");
-		String name = scanner.nextLine();
-		System.out.print("Enter price per item: ");
-		double price = scanner.nextDouble();
-		System.out.print("Enter initial quantity: ");
-		int quantity = scanner.nextInt();
+        System.out.println("--- Setup Your Shopping Cart Item ---");
+        System.out.print("Enter item name: ");
+        String enteredItemName = inputScanner.nextLine();
+        System.out.print("Enter price per item: ");
+        double enteredItemPrice = inputScanner.nextDouble();
+        System.out.print("Enter initial quantity: ");
+        int enteredItemQuantity = inputScanner.nextInt();
 
-		CartItem item = new CartItem(name, price, quantity);
-		int choice;
+        CartItem cartObj = new CartItem(enteredItemName, enteredItemPrice, enteredItemQuantity);
+        int menuChoice;
 
-		do {
-			item.displayItem();
-			System.out.println("\n--- Cart Menu ---");
-			System.out.println("1. Add items");
-			System.out.println("2. Remove items");
-			System.out.println("3. Exit");
-			System.out.print("Choose an option: ");
-			choice = scanner.nextInt();
+        do {
+            cartObj.showCartDetails();
+            System.out.println("\n--- Cart Menu ---");
+            System.out.println("1. Add items");
+            System.out.println("2. Remove items");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            menuChoice = inputScanner.nextInt();
 
-			switch (choice) {
-				case 1:
-					System.out.print("Enter quantity to add: ");
-					int qtyToAdd = scanner.nextInt();
-					item.addItem(qtyToAdd);
-					break;
-				case 2:
-					System.out.print("Enter quantity to remove: ");
-					int qtyToRemove = scanner.nextInt();
-					item.removeItem(qtyToRemove);
-					break;
-				case 3:
-					System.out.println("Finalizing your cart. Thank you!");
-					break;
-				default:
-					System.out.println("Invalid option. Please try again.");
-			}
-		} while (choice != 3);
-		
-		item.displayItem();
-		scanner.close();
-	}
+            switch (menuChoice) {
+                case 1:
+                    System.out.print("Enter quantity to add: ");
+                    int qtyToAdd = inputScanner.nextInt();
+                    cartObj.addToCart(qtyToAdd);
+                    break;
+                case 2:
+                    System.out.print("Enter quantity to remove: ");
+                    int qtyToRemove = inputScanner.nextInt();
+                    cartObj.removeFromCart(qtyToRemove);
+                    break;
+                case 3:
+                    System.out.println("Finalizing your cart. Thank you!");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (menuChoice != 3);
+
+        cartObj.showCartDetails();
+        inputScanner.close();
+    }
 }
