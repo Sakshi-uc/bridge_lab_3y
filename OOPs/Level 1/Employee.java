960b7466ae4a196f55592e8d4cbfd63e31a9d1bf
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-19 22:52:22 +0530

    Update Employee.java

--- OOPs/Level 1/Employee.java
+++ OOPs/Level 1/Employee.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 
 public class Employee {
-	String name;
-	int id;
-	double salary;
-	
-	Employee(String name, int id, double salary){
-		this.name = name;
-		this.id = id;
-		this.salary = salary;
-	}
-	
-	public void displayEmployee() {
-		System.out.println("\n--- Employee Details ---");
-		System.out.println("Employee Name: " + name);
-		System.out.println("Employee id: " + id);
-		System.out.println("Employee Salary: " + salary);
-	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter Employee Name: ");
-		String name = scanner.nextLine();
-		
-		System.out.print("Enter Employee ID: ");
-		int id = scanner.nextInt();
-		
-		System.out.print("Enter Employee Salary: ");
-		double salary = scanner.nextDouble();
-		
-		Employee emp = new Employee(name, id, salary);
-		emp.displayEmployee();
-		
-		scanner.close();
-	}
+    String empName;
+    int empId;
+    double empSalary;
+
+    Employee(String empName, int empId, double empSalary) {
+        this.empName = empName;
+        this.empId = empId;
+        this.empSalary = empSalary;
+    }
+
+    public void showEmployeeDetails() {
+        System.out.println("\n--- Employee Details ---");
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Employee Salary: " + empSalary);
+    }
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.print("Enter Employee Name: ");
+        String enteredName = inputScanner.nextLine();
+
+        System.out.print("Enter Employee ID: ");
+        int enteredId = inputScanner.nextInt();
+
+        System.out.print("Enter Employee Salary: ");
+        double enteredSalary = inputScanner.nextDouble();
+
+        Employee empObj = new Employee(enteredName, enteredId, enteredSalary);
+        empObj.showEmployeeDetails();
+
+        inputScanner.close();
+    }
 }
