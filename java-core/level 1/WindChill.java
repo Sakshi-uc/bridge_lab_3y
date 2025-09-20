00f1754f02b1bdc07c04930eab9a9c15383369a8
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:09:54 +0530

    Update WindChill.java

--- java-core/level 1/WindChill.java
+++ java-core/level 1/WindChill.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class WindChill {
-    public static double calculateWindChill(double temp, double windSpeed) {
-        return 35.74 + 0.6215 * temp + (0.4275 * temp - 35.75) * Math.pow(windSpeed, 0.16);
+    public static double calculateWindChill(double tempValue, double windSpeedValue) {
+        return 35.74 + 0.6215 * tempValue + (0.4275 * tempValue - 35.75) * Math.pow(windSpeedValue, 0.16);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter temperature: ");
-        double temp = sc.nextDouble();
+        double temperature = sc.nextDouble();
         System.out.print("Enter wind speed: ");
         double windSpeed = sc.nextDouble();
 
-        System.out.println("Wind chill temperature: " + calculateWindChill(temp, windSpeed));
+        System.out.println("Wind chill temperature: " + calculateWindChill(temperature, windSpeed));
     }
 }
