df31c160e3b14a60af0184f4e5f5988369cb8d34
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:00:38 +0530

    Update CalculateSimpleInterest.java

--- Java_fundamentals/Java_fundamentals/CalculateSimpleInterest.java
+++ Java_fundamentals/Java_fundamentals/CalculateSimpleInterest.java
@@ -5,7 +5,7 @@ public class CalculateSimpleInterest {
         double principal = 1000.0;
         double rate = 5.0;
         double time = 3.0;
-        double simpleInterest = (principal * rate * time) / 100;
-        System.out.println("The simple interest for a principal of " + principal + ", rate of " + rate + "%, and time of " + time + " years is: " + simpleInterest);
+        double interest = principal * rate * time / 100;
+        System.out.println("Simple Interest: " + interest);
     }
 }
