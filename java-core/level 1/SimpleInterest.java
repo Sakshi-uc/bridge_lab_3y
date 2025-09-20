5cde37a0dd7405ecee12aafe042319c4ded1a0d8
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:02:37 +0530

    Update SimpleInterest.java

--- java-core/level 1/SimpleInterest.java
+++ java-core/level 1/SimpleInterest.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
 
 public class SimpleInterest {
-    public static double calculateSimpleInterest(double principal, double rate, double time) {
+    public static double calculate(double principal, double rate, double time) {
         return (principal * rate * time) / 100;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Principal: ");
+        System.out.print("Enter principal amount: ");
         double principal = sc.nextDouble();
-        System.out.print("Enter Rate: ");
+        System.out.print("Enter rate of interest (%): ");
         double rate = sc.nextDouble();
-        System.out.print("Enter Time: ");
+        System.out.print("Enter time in years: ");
         double time = sc.nextDouble();
 
-        double si = calculateSimpleInterest(principal, rate, time);
-        System.out.println("The Simple Interest is " + si + " for Principal " + principal +
-                           ", Rate of Interest " + rate + " and Time " + time);
+        double si = calculate(principal, rate, time);
+        System.out.println("Simple Interest: " + si);
     }
 }
