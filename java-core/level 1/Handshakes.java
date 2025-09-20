4d02be36c368cc20341007858828e8b71f9ebd13
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:00:22 +0530

    Update Handshakes.java

--- java-core/level 1/Handshakes.java
+++ java-core/level 1/Handshakes.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Handshakes {
     public static int calculateHandshakes(int n) {
-        return (n * (n - 1)) / 2;
+        return n * (n - 1) / 2;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of students: ");
-        int n = sc.nextInt();
-        System.out.println("Maximum number of handshakes: " + calculateHandshakes(n));
+        int n;
+        System.out.print("Number of students: ");
+        n = sc.nextInt();
+        System.out.println("Maximum handshakes: " + calculateHandshakes(n));
+        sc.close();
     }
 }
