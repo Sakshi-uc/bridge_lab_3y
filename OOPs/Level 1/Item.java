52620235e4d51d4dba4a192a25e13b121be7ee31
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:05:57 +0530

    Update Item.java

--- OOPs/Level 1/Item.java
+++ OOPs/Level 1/Item.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 
 public class Item {
-	String itemCode;
-	String itemName;
-	double price;
-	
-	Item(String itemName, String itemCode, double price){
-		this.itemCode = itemCode;
-		this.itemName = itemName;
-		this.price = price;
-	}
-	
-	public void displayItemDetails() {
-		System.out.println("\n--- Item Details ---");
-		System.out.println("Item Name: " + itemName);
-		System.out.println("Item Code: " + itemCode);
-		System.out.println("Item Price: " + price);
-	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter Item Name: ");
-		String itemName = scanner.nextLine();
-		
-		System.out.print("Enter Item Code: ");
-		String itemCode = scanner.nextLine();
-		
-		System.out.print("Enter Item Price: ");
-		double price = scanner.nextDouble();
-		
-		Item item = new Item(itemName, itemCode, price);
-		item.displayItemDetails();
-		
-		scanner.close();
-	}
+    String productCode;
+    String productName;
+    double productPrice;
+
+    Item(String productName, String productCode, double productPrice) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public void showItemDetails() {
+        System.out.println("\n--- Item Details ---");
+        System.out.println("Item Name: " + productName);
+        System.out.println("Item Code: " + productCode);
+        System.out.println("Item Price: " + productPrice);
+    }
+
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.print("Enter Item Name: ");
+        String enteredName = inputScanner.nextLine();
+
+        System.out.print("Enter Item Code: ");
+        String enteredCode = inputScanner.nextLine();
+
+        System.out.print("Enter Item Price: ");
+        double enteredPrice = inputScanner.nextDouble();
+
+        Item itemObj = new Item(enteredName, enteredCode, enteredPrice);
+        itemObj.showItemDetails();
+
+        inputScanner.close();
+    }
 }
