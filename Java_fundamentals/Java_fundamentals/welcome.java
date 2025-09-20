fbbaad9b7c169147b370c6615fe776d589fcbdc7
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:08:40 +0530

    Update welcome.java

--- Java_fundamentals/Java_fundamentals/welcome.java
+++ Java_fundamentals/Java_fundamentals/welcome.java
@@ -1,5 +1,6 @@
 package JavaFundamental;
-public class welcome {
+
+public class Welcome {
     public static void main(String[] args) {
         System.out.println("Welcome to Bridgelabz!");
     }
