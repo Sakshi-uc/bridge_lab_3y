c7fe4b463a4f588cc218e273be3f99758a475afc
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:19:49 +0530

    Update YoungestTallest.java

--- java-core/level 2/YoungestTallest.java
+++ java-core/level 2/YoungestTallest.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 
 public class YoungestTallest {
-    public static int findYoungest(int[] ages) {
-        int minIdx = 0;
-        for (int i = 1; i < ages.length; i++)
-            if (ages[i] < ages[minIdx]) minIdx = i;
-        return minIdx;
+    public static int getYoungestIndex(int[] ages) {
+        int youngestIdx = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < ages[youngestIdx]) youngestIdx = i;
+        }
+        return youngestIdx;
     }
 
-    public static int findTallest(int[] heights) {
-        int maxIdx = 0;
-        for (int i = 1; i < heights.length; i++)
-            if (heights[i] > heights[maxIdx]) maxIdx = i;
-        return maxIdx;
+    public static int getTallestIndex(int[] heights) {
+        int tallestIdx = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > heights[tallestIdx]) tallestIdx = i;
+        }
+        return tallestIdx;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] names = {"Amar", "Akbar", "Anthony"};
-        int[] ages = new int[3];
-        int[] heights = new int[3];
+        Scanner scanner = new Scanner(System.in);
+        String[] personNames = {"Amar", "Akbar", "Anthony"};
+        int[] personAges = new int[3];
+        int[] personHeights = new int[3];
+
         for (int i = 0; i < 3; i++) {
-            System.out.print("Enter age of " + names[i] + ": ");
-            ages[i] = sc.nextInt();
-            System.out.print("Enter height of " + names[i] + ": ");
-            heights[i] = sc.nextInt();
+            System.out.print("Enter age of " + personNames[i] + ": ");
+            personAges[i] = scanner.nextInt();
+            System.out.print("Enter height of " + personNames[i] + ": ");
+            personHeights[i] = scanner.nextInt();
         }
-        System.out.println("Youngest: " + names[findYoungest(ages)]);
-        System.out.println("Tallest: " + names[findTallest(heights)]);
+
+        System.out.println("Youngest: " + personNames[getYoungestIndex(personAges)]);
+        System.out.println("Tallest: " + personNames[getTallestIndex(personHeights)]);
     }
 }
