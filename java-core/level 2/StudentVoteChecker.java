436e033bc2ead6e6d899384bcce61bb2e1a4f506
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:18:25 +0530

    Update StudentVoteChecker.java

--- java-core/level 2/StudentVoteChecker.java
+++ java-core/level 2/StudentVoteChecker.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class StudentVoteChecker {
-    public static boolean canStudentVote(int age) {
-        if (age < 0) return false;
-        return age >= 18;
+    public static boolean canStudentVote(int studentAge) {
+        if (studentAge < 0) return false;
+        return studentAge >= 18;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] ages = new int[10];
+        Scanner input = new Scanner(System.in);
+        int[] studentAges = new int[10];
         for (int i = 0; i < 10; i++) {
-            System.out.print("Enter age of student " + (i+1) + ": ");
-            ages[i] = sc.nextInt();
-            System.out.println(canStudentVote(ages[i]) ? "Can Vote" : "Cannot Vote");
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            studentAges[i] = input.nextInt();
+            System.out.println(canStudentVote(studentAges[i]) ? "Can Vote" : "Cannot Vote");
         }
     }
 }
