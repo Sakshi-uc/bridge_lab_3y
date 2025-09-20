7916cabecaacfe1808efa592e42e6c5ace979045
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:07:12 +0530

    Update MobilePhone.java

--- OOPs/Level 1/MobilePhone.java
+++ OOPs/Level 1/MobilePhone.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 
 public class MobilePhone {
-	String brand;
-	String model;
-	double price;
-	
-	MobilePhone(String brand, String model, double price){
-		this.brand = brand;
-		this.model = model;
-		this.price = price;
-	}
-	
-	public void diplayMobile() {
-		System.out.println("\n--- Mobile Phone Details ---");
-		System.out.println("Brand of mobile: " + brand);
-		System.out.println("Model of mobile: " + model);
-		System.out.println("Price of mobile: " + price);
-	}
-	
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("Enter mobile brand: ");
-		String brand = scanner.nextLine();
-		
-		System.out.print("Enter mobile model: ");
-		String model = scanner.nextLine();
-		
-		System.out.print("Enter mobile price: ");
-		double price = scanner.nextDouble();
-		
-		MobilePhone mobile = new MobilePhone(brand, model, price);
-		mobile.diplayMobile();
-		
-		scanner.close();
-	}
+    String phoneBrand;
+    String phoneModel;
+    double phonePrice;
+    
+    MobilePhone(String phoneBrand, String phoneModel, double phonePrice){
+        this.phoneBrand = phoneBrand;
+        this.phoneModel = phoneModel;
+        this.phonePrice = phonePrice;
+    }
+    
+    public void showMobileDetails() {
+        System.out.println("\n--- Mobile Phone Details ---");
+        System.out.println("Brand: " + phoneBrand);
+        System.out.println("Model: " + phoneModel);
+        System.out.println("Price: " + phonePrice);
+    }
+    
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        
+        System.out.print("Enter mobile brand: ");
+        String enteredBrand = inputScanner.nextLine();
+        
+        System.out.print("Enter mobile model: ");
+        String enteredModel = inputScanner.nextLine();
+        
+        System.out.print("Enter mobile price: ");
+        double enteredPrice = inputScanner.nextDouble();
+        
+        MobilePhone mobileObj = new MobilePhone(enteredBrand, enteredModel, enteredPrice);
+        mobileObj.showMobileDetails();
+        
+        inputScanner.close();
+    }
 }
