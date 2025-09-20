bfc1327d6963d6b8e92dabfb9edc96fbbafc332d
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:49:09 +0530

    Update AnimalTest.java

--- inheritance/Level_1/AnimalTest.java
+++ inheritance/Level_1/AnimalTest.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.inheritance;
+
 class Animal {
     String name;
     int age;
@@ -10,21 +11,29 @@ class Animal {
         System.out.println("Animal makes a sound");
     }
 }
+
 class Dog extends Animal {
     Dog(String name, int age) { super(name, age); }
     @Override void makeSound() { System.out.println("Woof!"); }
 }
+
 class Cat extends Animal {
     Cat(String name, int age) { super(name, age); }
     @Override void makeSound() { System.out.println("Meow!"); }
 }
+
 class Bird extends Animal {
     Bird(String name, int age) { super(name, age); }
     @Override void makeSound() { System.out.println("Chirp!"); }
 }
+
 public class AnimalTest {
     public static void main(String[] args) {
-        Animal[] animals = { new Dog("Rex", 3), new Cat("Kitty", 2), new Bird("Tweety", 1) };
+        Animal[] animals = {
+            new Dog("Rex", 3),
+            new Cat("Kitty", 2),
+            new Bird("Tweety", 1)
+        };
         for (Animal a : animals) {
             System.out.print(a.name + " says: ");
             a.makeSound();
