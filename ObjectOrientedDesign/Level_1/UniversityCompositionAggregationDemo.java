aa202a2962a9648bc964f38ac1e7288f67cd4ac8
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:46:50 +0530

    Update UniversityCompositionAggregationDemo.java

--- ObjectOrientedDesign/Level_1/UniversityCompositionAggregationDemo.java
+++ ObjectOrientedDesign/Level_1/UniversityCompositionAggregationDemo.java
@@ -1,41 +1,41 @@
 import java.util.*;
 
 class Faculty {
-    private String name;
-    public Faculty(String name){ this.name = name; }
-    public String getName(){ return name; }
+    private String facultyName;
+    public Faculty(String facultyName) { this.facultyName = facultyName; }
+    public String getName() { return facultyName; }
 }
 
 class DepartmentU {
-    private String deptName;
-    public DepartmentU(String name){ this.deptName = name; }
-    public String getDeptName(){ return deptName; }
+    private String departmentName;
+    public DepartmentU(String departmentName) { this.departmentName = departmentName; }
+    public String getDeptName() { return departmentName; }
 }
 
 class University {
-    private String name;
-    private List<DepartmentU> departments = new ArrayList<>();
-    private List<Faculty> faculties = new ArrayList<>();
-    public University(String name){ this.name = name; }
-    public void addDepartment(DepartmentU d){ departments.add(d); }
-    public void addFaculty(Faculty f){ faculties.add(f); }
-    public void showDetails(){
-        System.out.println("University: " + name);
+    private String universityName;
+    private List<DepartmentU> departmentList = new ArrayList<>();
+    private List<Faculty> facultyList = new ArrayList<>();
+    public University(String universityName) { this.universityName = universityName; }
+    public void addDepartment(DepartmentU dept) { departmentList.add(dept); }
+    public void addFaculty(Faculty fac) { facultyList.add(fac); }
+    public void showDetails() {
+        System.out.println("University: " + universityName);
         System.out.println("Departments:");
-        for(DepartmentU d: departments) System.out.println(" - " + d.getDeptName());
+        for (DepartmentU d : departmentList) System.out.println(" - " + d.getDeptName());
         System.out.println("Faculties:");
-        for(Faculty f: faculties) System.out.println(" - " + f.getName());
+        for (Faculty f : facultyList) System.out.println(" - " + f.getName());
     }
 }
 
 public class UniversityCompositionAggregationDemo {
     public static void main(String[] args) {
-        University u = new University("Delhi University");
-        u.addDepartment(new DepartmentU("CS"));
-        u.addDepartment(new DepartmentU("Physics"));
-        u.addFaculty(new Faculty("Dr. Mehta"));
-        u.addFaculty(new Faculty("Prof. Rao"));
+        University uni = new University("Delhi University");
+        uni.addDepartment(new DepartmentU("CS"));
+        uni.addDepartment(new DepartmentU("Physics"));
+        uni.addFaculty(new Faculty("Dr. Mehta"));
+        uni.addFaculty(new Faculty("Prof. Rao"));
 
-        u.showDetails();
+        uni.showDetails();
     }
 }
