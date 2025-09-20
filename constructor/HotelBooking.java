73262c940dfa37a0997de2481969d087fa6a44b0
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:48:44 +0530

    Update HotelBooking.java

--- constructor/HotelBooking.java
+++ constructor/HotelBooking.java
@@ -3,21 +3,18 @@ public class HotelBooking {
     private String roomType;
     private int nights;
 
-    
     public HotelBooking() {
         this.guestName = "Unknown";
         this.roomType = "Standard";
         this.nights = 1;
     }
 
-    
     public HotelBooking(String guestName, String roomType, int nights) {
         this.guestName = guestName;
         this.roomType = roomType;
         this.nights = nights;
     }
 
-   
     public HotelBooking(HotelBooking other) {
         this.guestName = other.guestName;
         this.roomType = other.roomType;
