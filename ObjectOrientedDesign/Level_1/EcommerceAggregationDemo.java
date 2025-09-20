5a937ad767f979e364c8809917414152650b6f31
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:18:27 +0530

    Update EcommerceAggregationDemo.java

--- ObjectOrientedDesign/Level_1/EcommerceAggregationDemo.java
+++ ObjectOrientedDesign/Level_1/EcommerceAggregationDemo.java
@@ -1,38 +1,49 @@
 import java.util.*;
 
 class Product {
-    private String name;
-    private double price;
-    public Product(String name,double price){ this.name=name; this.price=price; }
-    public String getName(){ return name; }
-    public double getPrice(){ return price; }
+    private String productName;
+    private double productPrice;
+
+    public Product(String productName, double productPrice){
+        this.productName = productName;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName(){ return productName; }
+    public double getProductPrice(){ return productPrice; }
 }
 
 class Order {
-    private List<Product> products = new ArrayList<>();
-    public void addProduct(Product p){ products.add(p); }
+    private List<Product> productList = new ArrayList<>();
+
+    public void addProduct(Product p){ productList.add(p); }
+
     public void showOrder(){
         System.out.println("Order contains:");
-        for(Product p: products) System.out.println(" - " + p.getName() + " $" + p.getPrice());
+        for(Product p: productList) {
+            System.out.println(" - " + p.getProductName() + " $" + p.getProductPrice());
+        }
     }
 }
 
 class Customer {
-    private String name;
-    public Customer(String name){ this.name=name; }
+    private String customerName;
+
+    public Customer(String name){ this.customerName = name; }
+
     public void placeOrder(Order o){
-        System.out.println(name + " placed an order");
+        System.out.println(customerName + " placed an order:");
         o.showOrder();
     }
 }
 
 public class EcommerceAggregationDemo {
     public static void main(String[] args) {
-        Customer c = new Customer("Ravi");
-        Order o = new Order();
-        o.addProduct(new Product("Laptop",50000));
-        o.addProduct(new Product("Mouse",800));
+        Customer customer = new Customer("Ravi");
+        Order order = new Order();
+        order.addProduct(new Product("Laptop", 50000));
+        order.addProduct(new Product("Mouse", 800));
 
-        c.placeOrder(o);
+        customer.placeOrder(order);
     }
 }
