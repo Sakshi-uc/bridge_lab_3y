5a7ea18e8eeb40120d17227ea67b6a5100016eda
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:43:14 +0530

    Update SchoolAggregationAssociationDemo.java

--- ObjectOrientedDesign/Level_1/SchoolAggregationAssociationDemo.java
+++ ObjectOrientedDesign/Level_1/SchoolAggregationAssociationDemo.java
@@ -3,58 +3,68 @@ import java.util.*;
 class Course {
     private String courseName;
     private List<Student> students = new ArrayList<>();
+
     public Course(String courseName) { this.courseName = courseName; }
-    public void enrollStudent(Student s) {
-        students.add(s);
-        s.addCourse(this);
+
+    public void enrollStudent(Student student) {
+        students.add(student);
+        student.addCourse(this);
     }
+
     public void showEnrolled() {
         System.out.println("Course: " + courseName);
-        for (Student s : students) System.out.println(" - " + s.getName());
+        for (Student student : students) System.out.println(" - " + student.getName());
     }
 }
 
 class Student {
     private String name;
     private List<Course> courses = new ArrayList<>();
+
     public Student(String name) { this.name = name; }
+
     public String getName() { return name; }
-    public void addCourse(Course c) { courses.add(c); }
+
+    public void addCourse(Course course) { courses.add(course); }
+
     public void showCourses() {
         System.out.println(name + " enrolled in:");
-        for (Course c : courses) System.out.println(" - " + c.courseName);
+        for (Course course : courses) System.out.println(" - " + course.courseName);
     }
 }
 
 class School {
     private String name;
     private List<Student> students = new ArrayList<>();
+
     public School(String name) { this.name = name; }
-    public void addStudent(Student s) { students.add(s); }
+
+    public void addStudent(Student student) { students.add(student); }
+
     public void showStudents() {
         System.out.println("School: " + name);
-        for (Student s : students) System.out.println(" - " + s.getName());
+        for (Student student : students) System.out.println(" - " + student.getName());
     }
 }
 
 public class SchoolAggregationAssociationDemo {
     public static void main(String[] args) {
         School school = new School("Springfield High");
-        Student s1 = new Student("Rahul");
-        Student s2 = new Student("Priya");
+        Student student1 = new Student("Rahul");
+        Student student2 = new Student("Priya");
 
-        school.addStudent(s1);
-        school.addStudent(s2);
+        school.addStudent(student1);
+        school.addStudent(student2);
 
-        Course c1 = new Course("Math");
-        Course c2 = new Course("Science");
+        Course course1 = new Course("Math");
+        Course course2 = new Course("Science");
 
-        c1.enrollStudent(s1);
-        c2.enrollStudent(s1);
-        c2.enrollStudent(s2);
+        course1.enrollStudent(student1);
+        course2.enrollStudent(student1);
+        course2.enrollStudent(student2);
 
         school.showStudents();
-        s1.showCourses();
-        c2.showEnrolled();
+        student1.showCourses();
+        course2.showEnrolled();
     }
 }
