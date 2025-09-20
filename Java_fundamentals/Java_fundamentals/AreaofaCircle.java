288259656991e5c34485d03b37dbe92153d35d3a
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 12:59:33 +0530

    Update AreaofaCircle.java

--- Java_fundamentals/Java_fundamentals/AreaofaCircle.java
+++ Java_fundamentals/Java_fundamentals/AreaofaCircle.java
@@ -1,9 +1,9 @@
 package JavaFundamental;
 
-public class AreaofaCircle {
+public class AreaOfCircle {
     public static void main(String[] args) {
         double radius = 5.0;
-        double area = Math.PI * radius * radius;
-        System.out.println("The area of the circle with radius " + radius + " is: " + area);
+        double area = Math.PI * Math.pow(radius, 2);
+        System.out.println("Area of circle with radius " + radius + " = " + area);
     }
 }
