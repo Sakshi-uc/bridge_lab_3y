645837fafc90f1efce9499e71342ee5b8e4a5e47
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:12:43 +0530

    Update FactorsProgram.java

--- java-core/level 2/FactorsProgram.java
+++ java-core/level 2/FactorsProgram.java
@@ -1,43 +1,46 @@
 import java.util.Scanner;
 
 public class FactorsProgram {
-    public static int[] findFactors(int n) {
+    public static int[] getFactors(int number) {
         int count = 0;
-        for (int i = 1; i <= n; i++) if (n % i == 0) count++;
+        for (int i = 1; i <= number; i++) if (number % i == 0) count++;
         int[] factors = new int[count];
-        int idx = 0;
-        for (int i = 1; i <= n; i++) if (n % i == 0) factors[idx++] = i;
+        int index = 0;
+        for (int i = 1; i <= number; i++) if (number % i == 0) factors[index++] = i;
         return factors;
     }
 
-    public static int sumFactors(int[] arr) {
+    public static int sumOfFactors(int[] factorArr) {
         int sum = 0;
-        for (int f : arr) sum += f;
+        for (int factor : factorArr) sum += factor;
         return sum;
     }
 
-    public static int productFactors(int[] arr) {
-        int prod = 1;
-        for (int f : arr) prod *= f;
-        return prod;
+    public static int productOfFactors(int[] factorArr) {
+        int product = 1;
+        for (int factor : factorArr) product *= factor;
+        return product;
     }
 
-    public static int sumOfSquares(int[] arr) {
+    public static int sumSquaresOfFactors(int[] factorArr) {
         int sum = 0;
-        for (int f : arr) sum += Math.pow(f, 2);
+        for (int factor : factorArr) sum += factor * factor;
         return sum;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number: ");
-        int n = sc.nextInt();
-        int[] factors = findFactors(n);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int[] factors = getFactors(number);
+
         System.out.print("Factors: ");
         for (int f : factors) System.out.print(f + " ");
         System.out.println();
-        System.out.println("Sum = " + sumFactors(factors));
-        System.out.println("Product = " + productFactors(factors));
-        System.out.println("Sum of squares = " + sumOfSquares(factors));
+
+        System.out.println("Sum of factors = " + sumOfFactors(factors));
+        System.out.println("Product of factors = " + productOfFactors(factors));
+        System.out.println("Sum of squares of factors = " + sumSquaresOfFactors(factors));
     }
 }
