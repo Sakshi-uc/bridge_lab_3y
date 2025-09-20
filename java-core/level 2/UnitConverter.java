c5940439d1c27f1d6a6a045e316d5bd7c03fcc36
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:19:00 +0530

    Update UnitConverter.java

--- java-core/level 2/UnitConverter.java
+++ java-core/level 2/UnitConverter.java
@@ -1,17 +1,17 @@
 public class UnitConverter {
-    public static double convertKmToMiles(double km) { return km * 0.621371; }
-    public static double convertMilesToKm(double miles) { return miles * 1.60934; }
-    public static double convertMetersToFeet(double m) { return m * 3.28084; }
-    public static double convertFeetToMeters(double ft) { return ft * 0.3048; }
-    public static double convertYardsToFeet(double yd) { return yd * 3; }
-    public static double convertFeetToYards(double ft) { return ft * 0.333333; }
-    public static double convertMetersToInches(double m) { return m * 39.3701; }
-    public static double convertInchesToMeters(double in) { return in * 0.0254; }
-    public static double convertInchesToCm(double in) { return in * 2.54; }
-    public static double convertFahrenheitToCelsius(double f) { return (f - 32) * 5 / 9; }
-    public static double convertCelsiusToFahrenheit(double c) { return (c * 9 / 5) + 32; }
-    public static double convertPoundsToKg(double lb) { return lb * 0.453592; }
-    public static double convertKgToPounds(double kg) { return kg * 2.20462; }
-    public static double convertGallonsToLiters(double gal) { return gal * 3.78541; }
-    public static double convertLitersToGallons(double l) { return l * 0.264172; }
+    public static double kmToMiles(double kilometers) { return kilometers * 0.621371; }
+    public static double milesToKm(double miles) { return miles * 1.60934; }
+    public static double metersToFeet(double meters) { return meters * 3.28084; }
+    public static double feetToMeters(double feet) { return feet * 0.3048; }
+    public static double yardsToFeet(double yards) { return yards * 3; }
+    public static double feetToYards(double feet) { return feet * 0.333333; }
+    public static double metersToInches(double meters) { return meters * 39.3701; }
+    public static double inchesToMeters(double inches) { return inches * 0.0254; }
+    public static double inchesToCm(double inches) { return inches * 2.54; }
+    public static double fahrenheitToCelsius(double fahrenheit) { return (fahrenheit - 32) * 5 / 9; }
+    public static double celsiusToFahrenheit(double celsius) { return (celsius * 9 / 5) + 32; }
+    public static double poundsToKg(double pounds) { return pounds * 0.453592; }
+    public static double kgToPounds(double kg) { return kg * 2.20462; }
+    public static double gallonsToLiters(double gallons) { return gallons * 3.78541; }
+    public static double litersToGallons(double liters) { return liters * 0.264172; }
 }
