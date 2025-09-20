5b00e9200583770e846a3334c502ab85e44d5e8f
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:00:04 +0530

    Update CalculateAverageofThreeNumbers.java

--- Java_fundamentals/Java_fundamentals/CalculateAverageofThreeNumbers.java
+++ Java_fundamentals/Java_fundamentals/CalculateAverageofThreeNumbers.java
@@ -6,6 +6,6 @@ public class CalculateAverageofThreeNumbers {
         double num2 = 20.0;
         double num3 = 30.0;
         double average = (num1 + num2 + num3) / 3;
-        System.out.println("The average of " + num1 + ", " + num2 + ", and " + num3 + " is: " + average);
+        System.out.println("Average of " + num1 + ", " + num2 + ", " + num3 + " = " + average);
     }
 }
