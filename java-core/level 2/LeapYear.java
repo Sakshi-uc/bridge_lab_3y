ada3f66c982f036a20d4013076d26c9654c792b0
Sakshi Sharma <aashisharma484@gmail.com>
2025-09-20 13:13:22 +0530

    Update LeapYear.java

--- java-core/level 2/LeapYear.java
+++ java-core/level 2/LeapYear.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
 public class LeapYear {
-    public static boolean isLeapYear(int year) {
-        if (year < 1582) return false;
-        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    public static boolean checkLeapYear(int inputYear) {
+        if (inputYear < 1582) return false;
+        return (inputYear % 4 == 0 && inputYear % 100 != 0) || (inputYear % 400 == 0);
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter year: ");
-        int year = sc.nextInt();
-        if (isLeapYear(year)) System.out.println("Leap Year");
+        int yearEntered = scanner.nextInt();
+
+        if (checkLeapYear(yearEntered)) System.out.println("Leap Year");
         else System.out.println("Not a Leap Year");
     }
 }
